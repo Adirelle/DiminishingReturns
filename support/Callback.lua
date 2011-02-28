9cce3b8b5cc86bababd6e11d59fd3aee4b64dcb7
Adirelle <adirelle@tagada-team.net>
2011-02-28 12:59:20 +0100

    Support through callback simply return the addon version.

--- support/Callback.lua
+++ support/Callback.lua
@@ -4,11 +4,14 @@ if not addon then return end
 for index = 1, GetNumAddOns() do	
 	local callback = GetAddOnMetadata(index, 'X-DiminishingReturns-Callback')
 	if callback then
-		local parent = GetAddOnInfo(index)
+		local parent, version = GetAddOnInfo(index), GetAddOnMetadata(index, 'Version')
 		local func, msg = loadstring(callback, parent.." X-DiminishingReturns-Callback")
 		addon:Debug('X-DiminishingReturns-Callback for', parent, ' : ', callback)
 		if func then
-			addon:RegisterAddonSupport(parent, func)
+			addon:RegisterAddonSupport(parent, function()
+				func()
+				return 'supported', version
+			end)
 		else
 			geterrorhandler()(msg)
 		end
