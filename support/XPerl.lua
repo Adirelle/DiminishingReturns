cc1e9ef9713caf91c487821cb2a4ce216c2bbb4d
Adirelle <adirelle@gmail.com>
2014-11-24 07:58:02 +0100

    Fixed Z-Perl support.

--- support/XPerl.lua
+++ support/XPerl.lua
@@ -7,45 +7,45 @@ All rights reserved.
 local addon = _G.DiminishingReturns
 if not addon then return end
 
-local db
-
 local function XPerlSupport(addonName)
-	--<GLOBALS
-	local _G = _G
-	local GetAddOnMetadata = _G.GetAddOnMetadata
-	local gsub = _G.gsub
-	local hooksecurefunc = _G.hooksecurefunc
-	--GLOBALS>
+	return function()
+		--<GLOBALS
+		local _G = _G
+		local GetAddOnMetadata = _G.GetAddOnMetadata
+		local gsub = _G.gsub
+		local hooksecurefunc = _G.hooksecurefunc
+		--GLOBALS>
 
-	local db = addon.db:RegisterNamespace(addonName, {profile={
-		['*'] = {
-			enabled = true,
-			iconSize = 24,
-			direction = 'RIGHT',
-			spacing = 2,
-			anchorPoint = 'BOTTOMLEFT',
-			relPoint = 'TOPLEFT',
-			xOffset = 4,
-			yOffset = 4,
-		}
-	}})
+		local db = addon.db:RegisterNamespace(addonName, {profile={
+			['*'] = {
+				enabled = true,
+				iconSize = 24,
+				direction = 'RIGHT',
+				spacing = 2,
+				anchorPoint = 'BOTTOMLEFT',
+				relPoint = 'TOPLEFT',
+				xOffset = 4,
+				yOffset = 4,
+			}
+		}})
 
-	local function ucfirst(s)
-		return s:sub(1,1):upper()..s:sub(2)
-	end
+		local function ucfirst(s)
+			return s:sub(1,1):upper()..s:sub(2)
+		end
 
-	addon:RegisterCommonFrames(function(unit)
-		local refUnit = gsub(unit, "%d+$", "")
-		local function GetDatabase() return db.profile[refUnit], db end
-		addon:RegisterFrameConfig('XPerl: '..addon.L[refUnit], GetDatabase)
-		return addon:RegisterFrame('XPerl_'..ucfirst(unit), function(frame)
-			return addon:SpawnFrame(frame, frame, GetDatabase)
+		addon:RegisterCommonFrames(function(unit)
+			local refUnit = gsub(unit, "%d+$", "")
+			local function GetDatabase() return db.profile[refUnit], db end
+			addon:RegisterFrameConfig('XPerl: '..addon.L[refUnit], GetDatabase)
+			return addon:RegisterFrame('XPerl_'..ucfirst(unit), function(frame)
+				return addon:SpawnFrame(frame, frame, GetDatabase)
+			end)
 		end)
-	end)
 
-	hooksecurefunc('XPerl_SecureUnitButton_OnLoad', addon.CheckFrame)
+		hooksecurefunc('XPerl_SecureUnitButton_OnLoad', addon.CheckFrame)
 
-	return 'unknown', GetAddOnMetadata(addonName, 'Version')
+		return 'unknown', GetAddOnMetadata(addonName, 'Version')
+	end
 end
 
 addon:RegisterAddonSupport('XPerl', XPerlSupport('XPerl'))
